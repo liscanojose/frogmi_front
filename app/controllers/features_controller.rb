9a6a1188c452b5f612827b18e1bad58b75456cf8
Jose Liscano <jose@agendapro.cl>
2019-06-15 01:34:34 -0400

    agregado el input para la busqueda

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -2,6 +2,7 @@ class FeaturesController < ApplicationController
   require 'will_paginate/array'
   def index
     features = Feature.get_list_features(params[:mag_type])[:data]
+    p "features" + features.count.to_s
     @features = features.paginate(page: params[:page], per_page: 15)
   end
 
